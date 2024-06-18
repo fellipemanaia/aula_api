a60ae4cd0e06c78c1cfbe75362015ee8df0a36d5
Fellipe Manaia <100931530+fellipemanaia@users.noreply.github.com>
2024-06-18 09:47:50 -0300

    Add files via upload

---
+++ aula_api-main/api/data.php
@@ -0,0 +1,48 @@
+<?php 
+    return[
+        [
+            'nome' => 'Anderson',
+            'email' => 'anderson@email.com'
+        ],
+        [
+            'nome' => 'Fellipe',
+            'email' => 'fellipe@email.com'
+        ],
+        [
+            'nome' => 'Ana Laura',
+            'email' => 'analaura@email.com'
+        ],
+        [
+            'nome' => 'Lucas',
+            'email' => 'lucas@email.com'
+        ],
+        [
+            'nome' => 'Victor',
+            'email' => 'victor@email.com'
+        ],
+        [
+            'nome' => 'Ana Clara',
+            'email' => 'anaclara@email.com'
+        ],
+        [
+            'nome' => 'Nicolas',
+            'email' => 'nicolas@email.com'
+        ],
+        [
+            'nome' => 'David',
+            'email' => 'david@email.com'
+        ],
+        [
+            'nome' => 'Enzo',
+            'email' => 'enzo@email.com'
+        ],
+        [
+            'nome' => 'Thuany',
+            'email' => 'thuany@email.com'
+        ],
+        
+    ];
+
+
+
+?>
---
+++ aula_api-main/app/get_all_data/index.php
@@ -0,0 +1,9 @@
+<?php 
+require_once __DIR__. '/../../api/config.php';
+require_once __DIR__. '/../../api/response.php';
+
+    $data = require_once __DIR__. '/../../api/data.php';
+
+    echo Response::resposta(20,'sucesso',$data);
+
+?>
